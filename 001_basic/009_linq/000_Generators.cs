547d980e66aa6d64c52ecfda918e1a03410dcdc3
Michail Ershov <m.ershov@outlook.com>
2022-03-13 11:41:09 +0300

    edu

---
+++ 001_basic/009_linq/000_Generators.cs
@@ -0,0 +1,17 @@
+﻿namespace linq.LinqMethods
+{
+    public class Generators
+    {
+        public void RangeOfInteger()
+        {
+            int[] numbers = Enumerable.Range(0, 10).ToArray();
+            Array.ForEach(numbers, item => Console.WriteLine(item));
+        }
+
+        public void RepeatNumber()
+        {
+            int[] numbers = Enumerable.Repeat(7, 10).ToArray();
+            Array.ForEach(numbers, item => Console.WriteLine(item));
+        }
+    }
+}
---
+++ 001_basic/009_linq/004_JoinOperation.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq.LinqMethods
+{
+    internal class JoinOperation
+    {
+        public List<Product> GetProductList() => ProductData.ProductList;
+        public List<Customer> GetCustomerList() => CustomerData.CustomerList;
+
+        public void CrossJoinQuery()
+        {
+            #region cross-join
+            string[] categories = {
+                "Beverages",
+                "Condiments",
+                "Vegetables",
+                "Dairy Products",
+                "Seafood"
+            };
+
+            List<Product> products = GetProductList();
+
+            var q = from c in categories
+                    join p in products on c equals p.Category
+                    select (Category: c, p.ProductName);
+
+            foreach (var v in q)
+            {
+                Console.WriteLine(v.ProductName + ": " + v.Category);
+            }
+            #endregion           
+        }
+
+        public void GroupJoinQquery()
+        {
+            #region group-join
+            string[] categories = {
+                "Beverages",
+                "Condiments",
+                "Vegetables",
+                "Dairy Products",
+                "Seafood"
+            };
+
+            List<Product> products = GetProductList();
+
+            var q = from c in categories
+                    join p in products on c equals p.Category into ps
+                    select (Category: c, Products: ps);
+
+            foreach (var v in q)
+            {
+                Console.WriteLine(v.Category + ":");
+                foreach (var p in v.Products)
+                {
+                    Console.WriteLine("   " + p.ProductName);
+                }
+            }
+            #endregion            
+        }
+
+        public void CrossGroupJoin()
+        {
+            #region cross-group-join
+            string[] categories = {
+                "Beverages",
+                "Condiments",
+                "Vegetables",
+                "Dairy Products",
+                "Seafood"
+            };
+
+            List<Product> products = GetProductList();
+
+            var q = from c in categories
+                    join p in products on c equals p.Category into ps
+                    from p in ps
+                    select (Category: c, p.ProductName);
+
+            foreach (var v in q)
+            {
+                Console.WriteLine(v.ProductName + ": " + v.Category);
+            }
+            #endregion            
+        }
+
+        public void LeftOuterJoin()
+        {
+            #region left-outer-join
+            string[] categories = {
+                "Beverages",
+                "Condiments",
+                "Vegetables",
+                "Dairy Products",
+                "Seafood"
+            };
+
+            List<Product> products = GetProductList();
+
+            var q = from c in categories
+                    join p in products on c equals p.Category into ps
+                    from p in ps.DefaultIfEmpty()
+                    select (Category: c, ProductName: p == null ? "(No products)" : p.ProductName);
+
+            foreach (var v in q)
+            {
+                Console.WriteLine($"{v.ProductName}: {v.Category}");
+            }
+            #endregion            
+        }
+    }
+}
